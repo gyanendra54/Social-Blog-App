6a95c53f2de6952a68ab5f7e3ac3335e8da74410
gyanendra kumar bais <gyanedrabais54@gmail.com>
2024-01-18 12:52:38 +0530

    create user model

---
+++ api/models/user.model.js
@@ -0,0 +1,21 @@
+import { Timestamp } from "bson";
+import mongoose from "mongoose";
+const userSchema=new mongoose.Schema({
+    username:{
+        type: String, 
+        required: true,
+        unique: true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    passward:{
+        type: String,
+        required:true,
+    },
+},{Timestamp:true});
+
+const User=mongoose.model('User', userSchema);
+export default User;
