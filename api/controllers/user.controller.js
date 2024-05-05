e6f992609c2908de778eef5e05589609b36a7a57
gyanendra kumar bais <gyanedrabais54@gmail.com>
2024-05-05 14:31:54 +0530

    complete update user profile page functionality

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -34,22 +34,22 @@ export const updateUser = async (req, res, next) => {
       );
     }
     try {
-      const updatedUser = await User.findByIdAndUpdate(
-        req.params.userId,
-        {
-          $set: {
-            username: req.body.username,
-            email: req.body.email,
-            profilePicture: req.body.profilePicture,
-            password: req.body.password,
+        const updatedUser = await User.findByIdAndUpdate(
+          req.params.userId,
+          {
+            $set: {
+              username: req.body.username,
+              email: req.body.email,
+              profilePicture: req.body.profilePicture,
+              password: req.body.password,
+            },
           },
-        },
-        { new: true }
-      );
-      const { password, ...rest } = updatedUser._doc;
-      res.status(200).json(rest);
-    } catch (error) {
-      next(error);
+          { new: true }
+        );
+        const { password, ...rest } = updatedUser._doc;
+        res.status(200).json(rest);
+      } catch (error) {
+        next(error);
     }
   }
 };
