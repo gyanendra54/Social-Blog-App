d421b0edd7d77afc0dca22277baadbaf5a59c4bf
gyanendra kumar bais <gyanedrabais54@gmail.com>
2024-05-07 14:37:25 +0530

    complete create post functionality

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -22,7 +22,7 @@ const userSchema=new mongoose.Schema({
     },
     isAdmin: {
         type: Boolean,
-        default: false,
+        default: true,//only for demo
       },
 },{timestamps:true});
 
