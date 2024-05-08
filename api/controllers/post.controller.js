5995a5b94eb1f421384d47b9e90f093f76983425
gyanendra kumar bais <gyanedrabais54@gmail.com>
2024-05-08 14:09:10 +0530

    add update post functionality

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -2,7 +2,9 @@ import Post from '../models/post.model.js';
 import { errorHandler } from '../utiles/error.js';
 
 export const create = async (req, res, next) => {
-  
+  if (!req.user.isAdmin) {
+    return next(errorHandler(403, 'You are not allowed to create a post'));  
+  }
   if (!req.body.title || !req.body.content) {
     return next(errorHandler(400, 'Please provide all required fields'));
   }
@@ -76,6 +78,30 @@ export const deletepost = async (req, res, next) => {
   try {
     await Post.findByIdAndDelete(req.params.postId);
     res.status(200).json('The post has been deleted');
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updatepost = async (req, res, next) => {
+ 
+  if (!req.user.isAdmin || req.user.id !== req.params.userId) {
+    return next(errorHandler(403, 'You are not allowed to update this post'));
+  }
+  try {
+    const updatedPost = await Post.findByIdAndUpdate(
+      req.params.postId,
+      {
+        $set: {
+          title: req.body.title,
+          content: req.body.content,
+          category: req.body.category,
+          image: req.body.image,
+        },
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedPost);
   } catch (error) {
     next(error);
   }
