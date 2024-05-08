5f085c45919162672e10a1c0a71b1a8a66ccd734
gyanendra kumar bais <gyanedrabais54@gmail.com>
2024-05-08 14:58:30 +0530

    add call to action to the post page

---
+++ client/src/components/CallToAction.jsx
@@ -0,0 +1,24 @@
+import { Button } from 'flowbite-react';
+
+export default function CallToAction() {
+  return (
+    <div className='flex flex-col sm:flex-row p-3 border border-teal-500 justify-center items-center rounded-tl-3xl rounded-br-3xl text-center'>
+        <div className="flex-1 justify-center flex flex-col">
+            <h2 className='text-2xl'>
+                Want to learn more Project and Programming?
+            </h2>
+            <p className='text-gray-500 my-2'>
+                Checkout these resources Projects
+            </p>
+            <Button gradientDuoTone='purpleToPink' className='rounded-tl-xl rounded-bl-none'>
+                <a href="https://github.com/The-Cool-Coders/Project-Ideas-And-Resources" target='_blank' rel='noopener noreferrer'>
+                Top 100 Coding Projects For Beginners
+                </a>
+            </Button>
+        </div>
+        <div className="p-7 flex-1">
+            <img src="https://contentstatic.techgig.com/photo/76118246/5-personal-characteristics-of-successful-programmers.jpg?119058" />
+        </div>
+    </div>
+  )
+}
